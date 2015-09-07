cc7872e0091adc971efc3f5cb6dd306684255da1
Louvie <fushitaka@gmail.com>
2015-09-06 23:14:41 -0500

    Python code sample5

---
+++ sample5.py
@@ -0,0 +1,11 @@
+__author__ = 'Louvie'
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+
+for spot in a:
+    if spot in b:
+        c.append(spot)
+
+
+print(c)
